67e3d89e7cfac447ba90b640e2087aac390b403e
Victhor Souza <victhor.souza@dellead.com>
2021-02-19 00:18:57 -0300

    Refatoração changeSize

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-export { default as font } from './lib/font'
+import Font from './lib/font'
+
+export default{
+    changeFontSize: Font.changeFontSize,
+    _changeFontSize: Font._changeFontSize,
+    resetFontSize: Font.resetFontSize
+}
